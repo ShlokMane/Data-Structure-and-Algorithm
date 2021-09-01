515c0d202b71d1eee888445e5768ebfda33d1b9a
Shlok <76466839+ShlokMane@users.noreply.github.com>
2021-09-02 00:05:51 +0530

    Add files via upload

---
+++ 1_RevereseAnArray.cpp
@@ -0,0 +1,43 @@
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    int start=0,end=str.length()-1;
+    while(start<end)
+    {
+        char s=str[start];
+        str[start]=str[end];
+        str[end]=s;
+        start++;
+        end--;
+    }
+    return str;
+  //Your code here
+}
